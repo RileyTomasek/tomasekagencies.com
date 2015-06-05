98cbf6ab8a7bb8943d329506de5bb36320fdb68a
Riley Tomasek <riley.tomasek@gmail.com>
2015-06-05 16:24:06 -0400

    Restructure to work with github pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-  gulp.src('./app/styles/*.scss')
+  gulp.src('./styles/*.scss')
     .pipe(sass())
     .pipe(autoprefixer('last 1 version'))
     .pipe(cssmin())
-    .pipe(gulp.dest('./app/styles/'));
+    .pipe(gulp.dest('./styles/'));
 });
