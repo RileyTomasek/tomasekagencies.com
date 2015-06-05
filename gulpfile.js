c042f114d97b1af9eb4ee512700de496020d131d
Riley Tomasek <riley.tomasek@gmail.com>
2015-06-05 16:05:15 -0400

    Significantly simplify site All of the overhead from doing it the old way isn't worth the performance benefits for a small site like this...

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+  gulp.src('./app/styles/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 1 version'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./app/styles/'));
+});
