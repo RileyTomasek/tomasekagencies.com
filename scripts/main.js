761e50cf86d9db2ced63c980a46b90074ddc1cf9
Riley Tomasek <riley.tomasek@gmail.com>
2016-01-14 22:30:51 -0500

    Remove vancouver showroom

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 (function() {
     function initializeMaps() {
         var calgaryLatLng = new google.maps.LatLng(51.078767, -114.113933);
-        var vancouverLatLng = new google.maps.LatLng(49.269090, -123.081413);
 
         var calgaryMapOptions = {
             center: calgaryLatLng,
@@ -11,27 +10,13 @@
             zoomControl: true,
             scrollwheel: false
         };
-        var vancouverMapOptions = {
-            center: vancouverLatLng,
-            zoom: 12,
-            disableDefaultUI: true,
-            draggable: false,
-            zoomControl: true,
-            scrollwheel: false
-        };
 
         var calgaryMap = new google.maps.Map( document.getElementById("calgary-map-canvas"), calgaryMapOptions );
-        var vancouverMap = new google.maps.Map( document.getElementById("vancouver-map-canvas"), vancouverMapOptions );
 
         var calgaryMarker = new google.maps.Marker({
             position: calgaryLatLng,
             map: calgaryMap,
             title: 'Calgary Showroom'
-        });
-        var vancoverMarker = new google.maps.Marker({
-            position: vancouverLatLng,
-            map: vancouverMap,
-            title: 'Vancouver Showroom'
         });
     }
 
